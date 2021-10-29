ee87813a99135f2ea1926c7d8f607cccf0eef5a4
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-29 21:12:00 +0300

    add create profile for user

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -23,3 +23,25 @@ def my_profile(request):
     current_user=request.user
     profile =Profile.objects.get(username=current_user)
     return render(request,'profile/user_profile.html',{"profile":profile})
+
+    # profile username
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
+    
+    
+    # creating profile 
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        form =ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+    else:
+        form = ProfileForm()
+        return render(request,'profile/profile_form.html',{"form":form})
