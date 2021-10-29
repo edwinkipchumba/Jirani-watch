674f19b38d3fe4219adf7169b1a7145c13f3e526
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-29 20:57:51 +0300

    adding user request profile

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import *
+from .emails import *
 
 # Create your views here.
+
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+    return render(request,'index.html')
+
+    # user profile request
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
+    return render(request,'profile/user_profile.html',{"profile":profile})
