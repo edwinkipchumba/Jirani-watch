2ccbd1c023a87a9f53b888ea8129ab719d5fd7ee
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-30 15:16:54 +0300

    fix setting error

--- jirani/wsgi.py
+++ jirani/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Neighbours project.
+WSGI config for jirani project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Neighbours.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jirani.settings')
 
 application = get_wsgi_application()
